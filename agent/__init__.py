9c34dc9c77bae37b385646e446a0c23413e55144
James Vaisman <lavaisman@gmail.com>
2025-08-24 21:45:03 -0700

    Update __init__.py

--- agent/__init__.py
+++ agent/__init__.py
@@ -1,4 +1,4 @@
-# agent module init
 from .rag_retriever import LlamaRetriever
 from .generator import LlamaGenerator
-from .consistency_reward import ConsistencyRewardModel, RAGConsistencyRewardModel
+from .consistency import calc_consistency, ConsistencyMethod
+from .reward_model import RewardModel
